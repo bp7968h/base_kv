8ee788dcfa5779a82eecb201cefe76540a82fff6
Bhuwan Pandit <bp7968h@gre.ac.uk>
2024-07-02 23:36:40 +0100

    Add dependencies, project setup and general structure of API

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,81 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::HashMap;
+use std::fs::{File, OpenOptions};
+use serde_derive::{Deserialize, Serialize};
+use std::path::Path;
+use std::io::{self, prelude::*, BufReader, ErrorKind, SeekFrom};
+
+type  ByteString = Vec<u8>;
+type ByteStr = [u8];
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct KeyValuePair{
+    pub key: ByteString,
+    pub value: ByteString,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+//Maintains a mapping between keys and file locations
+#[derive(Debug)]
+pub struct BaseKV{
+    f: File,
+    pub index: HashMap<ByteString, u64>
+}
+
+impl BaseKV {
+    pub fn open(path: &Path) -> io::Result<Self> {
+        let f = OpenOptions::new()
+                    .read(true)
+                    .write(true)
+                    .create(true)
+                    .append(true)
+                    .open(path)?;
+        
+        let index = HashMap::new();
+
+        Ok(BaseKV{f, index})
+    }
+
+    pub fn load(&mut self) -> io::Result<()>{
+        let mut f = BufReader::new(&mut self.f);
+
+        loop {
+            let position = f.seek(SeekFrom::Current(0))?;
+            
+            let maybe_kv = BaseKV::process_record(&mut f);
+
+            let kv = match maybe_kv {
+                Ok(kv) => kv,
+                Err(err) => {
+                    match err.kind() {
+                        ErrorKind::UnexpectedEof => break,
+                        _ => return Err(err)
+                    }
+                }
+            };
+
+            self.index.insert(kv.key, position);
+        }
+
+        Ok(())
+    }
+
+    fn process_record<R: Read>(f: &mut R) -> io::Result<KeyValuePair> {
+        todo!();
+    }
+
+    pub fn get(&self, key: &ByteStr) -> io::Result<Option<ByteString>> {
+        todo!();
+    }
+
+    pub fn delete(&self, key: &ByteStr) -> io::Result<()> {
+        todo!();
+    }
+
+    pub fn insert(&self, key: &ByteStr, value: &ByteStr) -> io::Result<()> {
+        todo!();
+    }
+
+    pub fn update(&self, key: &ByteStr, value: &ByteStr) -> io::Result<()>  {
+        todo!();
     }
 }
